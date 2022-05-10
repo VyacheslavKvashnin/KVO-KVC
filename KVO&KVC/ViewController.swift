f8447831b721ce42d263495c86964502be235dbb
komplektan <54449303+komplektan@users.noreply.github.com>
2022-05-10 14:31:38 +0500

    The End

--- KVO&KVC/ViewController.swift
+++ KVO&KVC/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Person: NSObject {
+    @objc dynamic var name = String()
+    
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @objc let taylor = Person()
+    
+    @objc dynamic var inputText: String?
+    
+    var nameObservation: NSKeyValueObservation?
+    var inputTextObservation: NSKeyValueObservation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nameObservation = observe(\.taylor.name, options: [.new, .old]) { vc, change in
+            guard let updateName = change.newValue else { return }
+            vc.nameLabel.text = updateName
+            print(change.oldValue)
+        }
+        
+        inputTextObservation = observe(\.inputText, options: .new) { vc, change in
+            guard let inputTextUpdate = change.newValue as? String else { return }
+            vc.textLabel.text = inputTextUpdate
+        }
     }
 
-
+    @IBAction func pressButton(_ sender: Any) {
+        taylor.name = "Slava"
+    }
+    
+    @IBAction func textFieldEditing(_ sender: Any) {
+        inputText = textField.text
+    }
 }
 
